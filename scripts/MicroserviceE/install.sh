78e2727ebcdec6da50ebb9956194fee751961e0c
Ameya <ameyaadvankar@gmail.com>
2016-09-25 05:23:03 -0400

    #16 Fixing incorrect path

--- scripts/MicroserviceE/install.sh
+++ scripts/MicroserviceE/install.sh
@@ -1,5 +1,5 @@
-echo 'Copying MicroserviceC war to jetty webapps location' >> /var/log/sga-omni-jetty.log 2>&1
-cp /home/ubuntu/MicroserviceC/target/*.war /opt/jetty/webapps/
+echo 'Copying MicroserviceE war to jetty webapps location' >> /var/log/sga-omni-jetty.log 2>&1
+cp /home/ubuntu/MicroserviceE/target/*.war /opt/jetty/webapps/
 echo 'Starting jetty service' >> /var/log/sga-omni-jetty.log 2>&1
 sudo service jetty start >> /var/log/sga-omni-jetty.log 2>&1 &
 sleep 10
