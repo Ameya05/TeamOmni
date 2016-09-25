60045c434ec84210089e754c85421f58266b8639
Ameya <ameyaadvankar@gmail.com>
2016-09-25 04:26:26 -0400

    #21 Installed AWS CLI on EC2 instance. initiating build again.

--- scripts/MicroserviceC/beforeInstall.sh
+++ scripts/MicroserviceC/beforeInstall.sh
@@ -1,4 +1,5 @@
 echo 'Creating log file' >> /var/log/sga-apex-api-jetty.log 2>&1
 sudo touch /var/log/sga-omni-jetty.log
 echo 'killing jetty running instances' >> /var/log/sga-apex-api-jetty.log 2>&1
-sudo pkill -9 -f jetty >> /var/log/sga-apex-api-jetty.log 2>&1
+sudo pkill -9 -f jetty >> /var/log/sga-apex-api-jetty.log 2>&1 &
+sleep 10
