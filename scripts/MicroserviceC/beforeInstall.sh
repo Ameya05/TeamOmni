7ffedbceee1035a32d730e467ebe9f32d5aa4199
Ameya <ameyaadvankar@gmail.com>
2016-09-25 05:02:55 -0400

    #16: Fixed Codedeploy to log to correct file

--- scripts/MicroserviceC/beforeInstall.sh
+++ scripts/MicroserviceC/beforeInstall.sh
@@ -1,5 +1,4 @@
-echo 'Creating log file' >> /var/log/sga-apex-api-jetty.log 2>&1
+echo 'Creating log file' >> /var/log/sga-omni-jetty.log 2>&1
 sudo touch /var/log/sga-omni-jetty.log
-echo 'killing jetty running instances' >> /var/log/sga-apex-api-jetty.log 2>&1
-sudo pkill -9 -f jetty >> /var/log/sga-apex-api-jetty.log 2>&1 &
-sleep 10
+echo 'killing jetty running instances' >> /var/log/sga-omni-jetty.log 2>&1
+sudo pkill -9 -f jetty >> /var/log/sga-omni-jetty.log 2>&1 &
--- scripts/MicroserviceC/install.sh
+++ scripts/MicroserviceC/install.sh
@@ -1,5 +1,5 @@
-echo 'Copying MicroserviceC war to jetty webapps location' >> /var/log/sga-apex-api-jetty.log 2>&1
+echo 'Copying MicroserviceC war to jetty webapps location' >> /var/log/sga-omni-jetty.log 2>&1
 cp /home/ubuntu/MicroserviceC/target/*.war /opt/jetty/webapps/
-echo 'Starting jetty service' >> /var/log/sga-apex-api-jetty.log 2>&1
-sudo service jetty start >> /var/log/sga-apex-api-jetty.log 2>&1 &
+echo 'Starting jetty service' >> /var/log/sga-omni-jetty.log 2>&1
+sudo service jetty start >> /var/log/sga-omni-jetty.log 2>&1 &
 sleep 10
