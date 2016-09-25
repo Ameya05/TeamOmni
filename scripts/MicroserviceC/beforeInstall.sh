5aca0912ee4de7c930c74c82e890d227927d5a18
Ameya <ameyaadvankar@gmail.com>
2016-09-25 04:07:27 -0400

    #16 kicking off build with updated travis-ci credentials.

--- scripts/MicroserviceC/beforeInstall.sh
+++ scripts/MicroserviceC/beforeInstall.sh
@@ -2,4 +2,3 @@ echo 'Creating log file' >> /var/log/sga-apex-api-jetty.log 2>&1
 sudo touch /var/log/sga-omni-jetty.log
 echo 'killing jetty running instances' >> /var/log/sga-apex-api-jetty.log 2>&1
 sudo pkill -9 -f jetty >> /var/log/sga-apex-api-jetty.log 2>&1
-sleep 10
