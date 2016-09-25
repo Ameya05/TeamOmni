1484f24350e3295f88c93e57cc2325577b488e05
Ameya <ameyaadvankar@gmail.com>
2016-09-25 03:59:27 -0400

    #16 Modified access levels for some commands since the deployment is failing.

--- scripts/MicroserviceC/install.sh
+++ scripts/MicroserviceC/install.sh
@@ -1,6 +1,5 @@
-echo 'Copying MicroserviceC war to jetty webapps location'
-cd '/home/ubuntu/MicroserviceC/target'
-cp *.war /opt/jetty/webapps/
-echo 'Starting jetty service'
-sudo service jetty start
+echo 'Copying MicroserviceC war to jetty webapps location' >> /var/log/sga-apex-api-jetty.log 2>&1
+cp /home/ubuntu/MicroserviceC/target/*.war /opt/jetty/webapps/
+echo 'Starting jetty service' >> /var/log/sga-apex-api-jetty.log 2>&1
+sudo service jetty start >> /var/log/sga-apex-api-jetty.log 2>&1 &
 sleep 10
