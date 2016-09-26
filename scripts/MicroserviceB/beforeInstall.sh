85c1073456a7a9bf9bc2e14ef50ce4dd11b02fdc
karanshah126 <karanshah126@gmail.com>
2016-09-25 20:45:44 -0400

    #16 adding appspec and scripts to microservice B zip

--- scripts/MicroserviceB/beforeInstall.sh
+++ scripts/MicroserviceB/beforeInstall.sh
@@ -2,4 +2,4 @@ echo 'Creating log file' >> /var/log/sga-omni-python.log 2>&1
 
 sudo touch /var/log/sga-omni-python.log
 echo 'killing running python instances' >> /var/log/sga-omni-python.log 2>&1
-sudo pkill -9 -f python >> /var/log/sga-omni-python.log 2>&1 &
+ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/sga-omni-python.log 2>&1 &
