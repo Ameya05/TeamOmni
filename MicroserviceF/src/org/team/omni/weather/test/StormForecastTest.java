842db78a0c7f89396349ebcd424833f5b379bf07
karanshah126 <karanshah126@gmail.com>
2016-10-13 12:53:54 -0400

    #35 Adding JerseyTest to Microservice F

---
+++ MicroserviceF/src/org/team/omni/weather/test/StormForecastTest.java
@@ -0,0 +1,48 @@
+package org.team.omni.weather.test;
+
+import org.apache.commons.io.IOUtils;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.*;
+import org.team.omni.weather.service.StormForecastService;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+public class StormForecastTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(StormForecastService.class);
+    }
+
+    @Test
+    public void StormDetectionResponseStatusTest() {
+        int status = target("/run").request().get().getStatus();
+        assertEquals(200,status);
+    }
+    
+    @Test
+    public void StormDetectionResponseFileTest() {
+        	
+    	byte[] file1Bytes;
+		try {
+			 Response output = target("/run").request().get();
+			 InputStream in = (InputStream) output.getEntity();
+			 file1Bytes=IOUtils.toByteArray(in);
+	    	String file1 = new String(file1Bytes, StandardCharsets.UTF_8);
+	    	
+	    	assertNotNull(file1);
+	    	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+    }
+}
