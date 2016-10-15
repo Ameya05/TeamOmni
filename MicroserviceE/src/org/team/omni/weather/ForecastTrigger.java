91e781e1f3c1e9c780d8e9668efc7faab30ef365
karanshah126 <karanshah126@gmail.com>
2016-10-14 20:41:48 -0400

    #39 Adding logging to Microservice E MicroElogs.log in directory

--- MicroserviceE/src/org/team/omni/weather/ForecastTrigger.java
+++ MicroserviceE/src/org/team/omni/weather/ForecastTrigger.java
@@ -1,6 +1,7 @@
 package org.team.omni.weather;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Random;
@@ -12,17 +13,27 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.log4j.Logger;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
  
 @Path("/")
 public class ForecastTrigger {
+	
+
+
+	static { System.setProperty("my.log", System.getProperty("user.dir")
+            + File.separator + "MicroElog.log"); }
+	final static Logger logger = Logger.getLogger(ForecastTrigger.class);
+	
+	
 	@POST
 	@Path("/trigger")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	@Produces(MediaType.TEXT_PLAIN)
 	public Response sendCluster(@FormDataParam("clustering") InputStream incomingData) 
 	{
+		logger.info("Entered MicroserviceE");
 		StringBuilder builder = new StringBuilder();
 		try {
 			BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
@@ -31,6 +42,7 @@ public class ForecastTrigger {
 				builder.append(line);
 			}
 		} catch (Exception e) {
+			logger.error("StreamReaderError ");
 			System.out.println("Error Parsing: - ");
 		}
 		System.out.println("Data Received: " + builder.toString());
@@ -38,7 +50,7 @@ public class ForecastTrigger {
 		boolean trigger=false;
 		if (randomno.nextInt(2)==1)
 			trigger=true;
-		
+		logger.info("Returning to Orchestration Engine. Trigger Value: "+trigger);
 		return Response.status(200).entity(String.valueOf(trigger)).build();
 	}
 
