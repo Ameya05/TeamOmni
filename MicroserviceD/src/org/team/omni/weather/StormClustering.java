33965359d02a4bdd8b03ee1fa174dd35e42131d3
karanshah126 <karanshah126@gmail.com>
2016-10-14 20:24:50 -0400

    #39 Adding logging to Microservice D MicroDlogs.log in directory

--- MicroserviceD/src/org/team/omni/weather/StormClustering.java
+++ MicroserviceD/src/org/team/omni/weather/StormClustering.java
@@ -2,6 +2,7 @@
 package org.team.omni.weather;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -11,15 +12,24 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.log4j.Logger;
 import org.glassfish.jersey.media.multipart.FormDataParam;
  
 @Path("/")
 public class StormClustering {
+	
+	static { System.setProperty("my.log", System.getProperty("user.dir")
+            + File.separator + "MicroDlogs.log"); }
+	final static Logger logger = Logger.getLogger(StormClustering.class);
+	
 	@POST
 	@Path("/clustering")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	public Response sendCluster(@FormDataParam("kml") InputStream incomingData) {
 		StringBuilder builder = new StringBuilder();
+		
+		logger.info("Entered Microservice D");
+		
 		try {
 			BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
 			String line = null;
@@ -27,8 +37,10 @@ public class StormClustering {
 				builder.append(line);
 			}
 		} catch (Exception e) {
+			logger.error("StreamReaderError ");
 			System.out.println("Error Parsing: - ");
 		}
+		logger.info("returning response to Ochestration Engine");
 		return Response.status(200).entity(builder.toString()).build();
 	}
 }
