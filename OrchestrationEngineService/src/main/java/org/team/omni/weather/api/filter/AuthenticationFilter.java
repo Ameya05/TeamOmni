5e14e3b5b7a609be70a57b6a0f0a90a384add8cd
Ameya <ameyaadvankar@gmail.com>
2016-10-19 19:55:50 -0400

    #37 adding log statements

--- OrchestrationEngineService/src/main/java/org/team/omni/weather/api/filter/AuthenticationFilter.java
+++ OrchestrationEngineService/src/main/java/org/team/omni/weather/api/filter/AuthenticationFilter.java
@@ -1,6 +1,8 @@
 package org.team.omni.weather.api.filter;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
@@ -14,11 +16,16 @@ import org.team.omni.weather.api.services.AuthService;
 @Provider
 public class AuthenticationFilter implements ContainerRequestFilter
 {
+	private static final Logger LOGGER = Logger.getLogger("Orchestration");
+	
 	@Override
 	public void filter(ContainerRequestContext request) throws IOException 
 	{
 		MultivaluedMap<String, String> queryParameters = request.getUriInfo().getQueryParameters();
+		
 		String idToken = queryParameters.getFirst("idToken");
+		LOGGER.log(Level.INFO, "Size of queryParameters: "+queryParameters.size()+"\n Authenticating idToken: "+idToken);
+		
 		AuthService authService = new AuthService();
 		
 		if(idToken==null || !authService.authenticate(idToken))
