0a7da496b8ab01a366655091eb9df01bca76ee3f
Ameya <ameyaadvankar@gmail.com>
2016-10-21 18:19:35 -0400

    #50 fixed cache clearing

--- scripts/MicroserviceD/beforeInstall.sh
+++ scripts/MicroserviceD/beforeInstall.sh
@@ -2,4 +2,4 @@ touch /var/log/sga-docker.log 2>&1
 chmod 777 /var/log/sga-docker.log
 
 echo 'Removing existing docker instances' >> /var/log/sga-docker.log 2>&1
-sudo docker ps -a | grep '/omniStormClustering' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f /omniStormClustering
+sudo docker ps -a | grep 'omniStormClustering' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f /omniStormClustering
