07c7c74afb501eba0a6223bc55e4739bb8bbd877
Ameya <ameyaadvankar@gmail.com>
2016-10-21 17:48:48 -0400

    #50 executing scripts as sudo

--- scripts/MicroserviceC/beforeInstall.sh
+++ scripts/MicroserviceC/beforeInstall.sh
@@ -1,2 +1,2 @@
 echo 'Removing existing docker instances' >> /var/log/sga-docker.log 2>&1
-docker ps -a | grep 'omniStormDetector' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f omniStormDetector
+sudo docker ps -a | grep 'omniStormDetector' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f omniStormDetector
--- scripts/MicroserviceC/install.sh
+++ scripts/MicroserviceC/install.sh
@@ -1,4 +1,4 @@
 echo '===============Building docker===============' >> /var/log/sga-docker.log 2>&1
-docker build --build-arg APP_URL=https://s3-us-west-2.amazonaws.com/sga-team-omni/omniStormDetector.tar.gz -t omni/stormdetect:v1 . >> /var/log/sga-docker.log 2>&1
+sudo docker build --build-arg APP_URL=https://s3-us-west-2.amazonaws.com/sga-team-omni/omniStormDetector.tar.gz -t omni/stormdetect:v1 . >> /var/log/sga-docker.log 2>&1
 echo '===============Running docker===============' >> /var/log/sga-docker.log 2>&1
-docker run -it --name omniStormDetector -h storm.detector -p 8081:8080 -d omni/stormdetect:v1 >> /var/log/sga-docker.log 2>&1
+sudo docker run -it --name omniStormDetector -h storm.detector -p 8081:8080 -d omni/stormdetect:v1 >> /var/log/sga-docker.log 2>&1
