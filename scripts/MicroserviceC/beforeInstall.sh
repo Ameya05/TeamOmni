e34e5e7fe6e053a9d0799b72347aea2cec32f47c
Ameya <ameyaadvankar@gmail.com>
2016-10-20 23:47:37 -0400

    #50 Checking if docker is existing prior to removing it

--- scripts/MicroserviceC/beforeInstall.sh
+++ scripts/MicroserviceC/beforeInstall.sh
@@ -1,2 +1,2 @@
 echo 'Removing existing docker instances' >> /var/log/sga-docker.log 2>&1
-docker rm -f storm
+docker ps -a | grep 'omniStormDetector' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f omniStormDetector
