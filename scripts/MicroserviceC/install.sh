669e8f8da5d21936d749e105a6f1a27d69853a1b
Ameya <ameyaadvankar@gmail.com>
2016-10-20 23:33:37 -0400

    #50 Docker script addition in microservice C

--- scripts/MicroserviceC/install.sh
+++ scripts/MicroserviceC/install.sh
@@ -1,5 +1,4 @@
-echo 'Copying MicroserviceC war to jetty webapps location' >> /var/log/sga-omni-jetty.log 2>&1
-cp /home/ubuntu/MicroserviceC/target/*.war /opt/jetty/webapps/
-echo 'Starting jetty service' >> /var/log/sga-omni-jetty.log 2>&1
-sudo service jetty start >> /var/log/sga-omni-jetty.log 2>&1 &
-sleep 10
+echo '===============Building docker===============' >> /var/log/sga-docker.log 2>&1
+docker build --build-arg APP_URL=https://s3-us-west-2.amazonaws.com/sga-team-omni/omniStormDetector.tar.gz -t omni/storm:v1 . >> /var/log/sga-docker.log 2>&1
+echo '===============Running docker===============' >> /var/log/sga-docker.log 2>&1
+docker run --name storm -h storm.post -e APP_URL=https://s3-us-west-2.amazonaws.com/sga-team-omni/omniStormDetector.tar.gz -p 8080:8080 -d -it omni/storm:v1 >> /var/log/sga-docker.log 2>&1
