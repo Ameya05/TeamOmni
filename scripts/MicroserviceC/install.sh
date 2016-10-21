8e664eb4f19a89f4d8e5665f9d808fb8ae6f5c69
Ameya <ameyaadvankar@gmail.com>
2016-10-21 15:16:43 -0400

    #50 moved dockerfile to root folder and changed install.sh to expose port 8081

--- scripts/MicroserviceC/install.sh
+++ scripts/MicroserviceC/install.sh
@@ -1,4 +1,4 @@
 echo '===============Building docker===============' >> /var/log/sga-docker.log 2>&1
 docker build --build-arg APP_URL=https://s3-us-west-2.amazonaws.com/sga-team-omni/omniStormDetector.tar.gz -t omniStormDetector:v1 . >> /var/log/sga-docker.log 2>&1
 echo '===============Running docker===============' >> /var/log/sga-docker.log 2>&1
-docker run --name omniStormDetector -h storm.detector -e APP_URL=https://s3-us-west-2.amazonaws.com/sga-team-omni/omniStormDetector.tar.gz -p 8080:8080 -d -it omniStormDetector:v1 >> /var/log/sga-docker.log 2>&1 &
+docker run --name omniStormDetector -h storm.detector -e APP_URL=https://s3-us-west-2.amazonaws.com/sga-team-omni/omniStormDetector.tar.gz -p 8081:8080 -d -it omniStormDetector:v1 >> /var/log/sga-docker.log 2>&1 &
