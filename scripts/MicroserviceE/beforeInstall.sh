6f4b9a9867e463f71f03a3815a959fecd5e33fbf
Ameya <ameyaadvankar@gmail.com>
2016-10-22 19:23:44 -0400

    #53 Correcting scripts and logging for Microservice E

--- scripts/MicroserviceE/beforeInstall.sh
+++ scripts/MicroserviceE/beforeInstall.sh
@@ -1,5 +1,5 @@
 sudo touch /var/log/sga-docker.log
-sudo chmod 777 var/log/sga-docker.log
+sudo chmod 777 /var/log/sga-docker.log
 
 echo 'Removing existing docker instances for forecast-trigger-service' >> /var/log/sga-docker.log 2>&1
 sudo docker ps -a | grep 'forecast-trigger-service' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f
