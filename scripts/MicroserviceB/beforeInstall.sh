83a8c8a8e8d99814cbd648277aaafa55853f798a
Ameya <ameyaadvankar@gmail.com>
2016-10-22 19:30:02 -0400

    #53 Correcting logging and scripts for Microservice B

--- scripts/MicroserviceB/beforeInstall.sh
+++ scripts/MicroserviceB/beforeInstall.sh
@@ -1,2 +1,5 @@
-echo 'Removing existing docker instances' >> /var/log/sga-docker.log 2>&1
-docker ps -a | grep 'data-ingestor-service' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f data-ingestor-service
+sudo touch /var/log/sga-docker.log
+sudo chmod 777 /var/log/sga-docker.log
+
+echo 'Removing existing docker instances for data-ingestor-service' >> /var/log/sga-docker.log 2>&1
+docker ps -a | grep 'data-ingestor-service' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f
--- scripts/MicroserviceB/install.sh
+++ scripts/MicroserviceB/install.sh
@@ -1,4 +1,6 @@
-echo '===============Building docker===============' >> /var/log/sga-docker.log 2>&1
+
+cd /home/ubuntu/
+echo '===============Building data-ingestor-service docker===============' >> /var/log/sga-docker.log 2>&1
 docker build --build-arg APP_URL=https://s3-us-west-2.amazonaws.com/sga-team-omni/omniDataIngestor.tar.gz -t omni/python:v1 . >> /var/log/sga-docker.log 2>&1
-echo '===============Running docker===============' >> /var/log/sga-docker.log 2>&1
-docker run -it --name data-ingestor-service -h omni.python -p 65000:65000 -d omni/python:v1
+echo '===============Running data-ingestor-service docker===============' >> /var/log/sga-docker.log 2>&1
+docker run -it --name 'data-ingestor-service' -h omni.python -p 65000:65000 -d omni/python:v1 >> /var/log/sga-docker.log 2>&1
