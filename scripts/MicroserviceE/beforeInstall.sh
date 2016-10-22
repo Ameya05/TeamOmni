93848acc4e2b675899d7777b5aa4685834e3b112
Ameya <ameyaadvankar@gmail.com>
2016-10-22 18:51:49 -0400

    #52 Tweaked scripts and deployed microservice E to new instance

--- scripts/MicroserviceE/beforeInstall.sh
+++ scripts/MicroserviceE/beforeInstall.sh
@@ -1,2 +1,5 @@
-echo 'Removing existing docker instances' >> /var/log/sga-docker.log 2>&1
-sudo docker ps -a | grep 'forecast-trigger-service' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f forecast-trigger-service
+sudo touch /var/log/sga-docker.log
+sudo chmod 777 var/log/sga-docker.log
+
+echo 'Removing existing docker instances for forecast-trigger-service' >> /var/log/sga-docker.log 2>&1
+sudo docker ps -a | grep 'forecast-trigger-service' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f
--- scripts/MicroserviceE/install.sh
+++ scripts/MicroserviceE/install.sh
@@ -1,4 +1,4 @@
-echo '===============Building docker===============' >> /var/log/sga-docker.log 2>&1
+echo '===============Building forecast-trigger-service docker ===============' >> /var/log/sga-docker.log 2>&1
 sudo docker build --build-arg APP_URL=https://s3-us-west-2.amazonaws.com/sga-team-omni/omniForecastTrigger.tar.gz -t omni/forecast:v1 . >> /var/log/sga-docker.log 2>&1
-echo '===============Running docker===============' >> /var/log/sga-docker.log 2>&1
+echo '===============Running forecast-trigger-service docker===============' >> /var/log/sga-docker.log 2>&1
 sudo docker run -it --name 'forecast-trigger-service' -h storm.forecast -p 8083:8080 -d omni/forecast:v1 >> /var/log/sga-docker.log 2>&1
