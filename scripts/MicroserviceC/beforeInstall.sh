684e22d0b1bc8a60d08b5ff6552178edd36800f8
Ameya <ameyaadvankar@gmail.com>
2016-10-22 18:40:14 -0400

    #52 Fixed scripts to do a clean install of docker container

--- scripts/MicroserviceC/beforeInstall.sh
+++ scripts/MicroserviceC/beforeInstall.sh
@@ -1,2 +1,5 @@
+sudo touch /var/log/sga-docker.log
+sudo chmod 777 var/log/sga-docker.log
+
 echo 'Removing existing docker instances' >> /var/log/sga-docker.log 2>&1
-sudo docker ps -a | grep 'omniStormDetector' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f omniStormDetector
+sudo docker ps -a | grep 'omniStormDetector' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f
