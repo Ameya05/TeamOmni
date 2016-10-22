ba1bcb835273801403a91a374bfc346f7525f905
Ameya <ameyaadvankar@gmail.com>
2016-10-22 19:25:03 -0400

    #53 Correcting logging and scripts for Microservice D

--- scripts/MicroserviceD/beforeInstall.sh
+++ scripts/MicroserviceD/beforeInstall.sh
@@ -1,4 +1,4 @@
-sudo touch /var/log/sga-docker.log 2>&1
+sudo touch /var/log/sga-docker.log
 sudo chmod 777 /var/log/sga-docker.log
 
 echo 'Removing existing docker instances for omniStormClustering' >> /var/log/sga-docker.log 2>&1
--- scripts/MicroserviceD/install.sh
+++ scripts/MicroserviceD/install.sh
@@ -1,3 +1,5 @@
+
+cd /home/ubuntu/
 echo '===============Building omniStormClustering docker===============' >> /var/log/sga-docker.log 2>&1
 sudo docker build --build-arg APP_URL=https://s3-us-west-2.amazonaws.com/sga-team-omni/omniStormClustering.tar.gz -t omni/stormcluster:v1 . >> /var/log/sga-docker.log 2>&1
 echo '===============Running omniStormClustering docker===============' >> /var/log/sga-docker.log 2>&1
