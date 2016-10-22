3a8d498dbbcd04e4127666588bd92e678796a691
Ameya <ameyaadvankar@gmail.com>
2016-10-22 18:46:05 -0400

    #52 Tweaked scripts and deploying Microservice D to new instance

--- MicroserviceD/src/org/team/omni/weather/test/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author karan
- *
- */
-package org.team.omni.weather.test;
--- scripts/MicroserviceD/beforeInstall.sh
+++ scripts/MicroserviceD/beforeInstall.sh
@@ -1,5 +1,5 @@
-touch /var/log/sga-docker.log 2>&1
-chmod 777 /var/log/sga-docker.log
+sudo touch /var/log/sga-docker.log 2>&1
+sudo chmod 777 /var/log/sga-docker.log
 
-echo 'Removing existing docker instances' >> /var/log/sga-docker.log 2>&1
-sudo docker ps -a | grep 'omniStormClustering' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f omniStormClustering
+echo 'Removing existing docker instances for omniStormClustering' >> /var/log/sga-docker.log 2>&1
+sudo docker ps -a | grep 'omniStormClustering' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f
