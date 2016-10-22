ac87418cbfdf4b2c58e2e0008959e73b0c942a7d
Ameya <ameyaadvankar@gmail.com>
2016-10-22 19:26:53 -0400

    #53 Correcting logging and scripts for Microservice B

--- scripts/MicroserviceC/install.sh
+++ scripts/MicroserviceC/install.sh
@@ -1,4 +1,6 @@
-echo '===============Building docker===============' >> /var/log/sga-docker.log 2>&1
+
+cd /home/ubuntu/
+echo '===============Building omniStormDetector docker===============' >> /var/log/sga-docker.log 2>&1
 sudo docker build --build-arg APP_URL=https://s3-us-west-2.amazonaws.com/sga-team-omni/omniStormDetector.tar.gz -t omni/stormdetect:v1 . >> /var/log/sga-docker.log 2>&1
-echo '===============Running docker===============' >> /var/log/sga-docker.log 2>&1
+echo '===============Running omniStormDetector docker===============' >> /var/log/sga-docker.log 2>&1
 sudo docker run -it --name omniStormDetector -h storm.detector -p 8081:8080 -d omni/stormdetect:v1 >> /var/log/sga-docker.log 2>&1
