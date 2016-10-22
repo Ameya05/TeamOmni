7daef909fa128cb8940b100a58680d4156ee999f
l2mathieu <eldhomathulla@gmail.com>
2016-10-21 23:06:44 -0400

    #50 Adding docker support for micro service f

--- scripts/MicroserviceF/install.sh
+++ scripts/MicroserviceF/install.sh
@@ -1,5 +1,4 @@
-echo 'Copying MicroserviceF war to jetty webapps location' >> /var/log/sga-omni-jetty.log 2>&1
-cp /home/ubuntu/MicroserviceF/target/*.war /opt/jetty/webapps/
-echo 'Starting jetty service' >> /var/log/sga-omni-jetty.log 2>&1
-sudo service jetty start >> /var/log/sga-omni-jetty.log 2>&1 &
-sleep 10
+echo '===============Building docker===============' >> /var/log/sga-docker.log 2>&1
+sudo docker build --build-arg APP_URL=https://s3-us-west-2.amazonaws.com/sga-team-omni/omniRunForecast.tar.gz -t omni/runforecast:v1 . >> /var/log/sga-docker.log 2>&1
+echo '===============Running docker===============' >> /var/log/sga-docker.log 2>&1
+sudo docker run -it --name 'run-forecast-service' -h storm.forecast.run -p 8084:8080 -d omni/runforecast:v1
