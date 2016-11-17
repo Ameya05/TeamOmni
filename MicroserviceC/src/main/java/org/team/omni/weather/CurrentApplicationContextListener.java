5a0d369370afb7bb9cbc748469aa66e6ab515362
Ameya <ameyaadvankar@gmail.com>
2016-11-17 17:12:29 -0500

    #70 Accessing env variable in code

--- MicroserviceC/src/main/java/org/team/omni/weather/CurrentApplicationContextListener.java
+++ MicroserviceC/src/main/java/org/team/omni/weather/CurrentApplicationContextListener.java
@@ -1,8 +1,9 @@
 package org.team.omni.weather;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.annotation.WebListener;
+import java.io.File;
 
+import javax.servlet.ServletContextEvent;
+import org.apache.log4j.Logger;
 /**
  * 
  * @author Eldho Mathulla
@@ -10,8 +11,15 @@ import javax.servlet.annotation.WebListener;
  */
 public class CurrentApplicationContextListener extends ApplicationContextListener {
 
+	static {
+		System.setProperty("my.log", System.getProperty("user.dir") + File.separator + "MicroClogs.log");
+	}
+	final static Logger logger = Logger.getLogger(CurrentApplicationContextListener.class);
+	
 	public CurrentApplicationContextListener() {
-		this.address = "localhost";
+		this.address = System.getenv("DOCKER_HOST");
+		logger.info("Set this.address: "+this.address);
+		
 		this.port = 8080;
 		this.maxWorkLoad = 100;
 		this.serviceName = "StormDetectionService";
@@ -24,7 +32,7 @@ public class CurrentApplicationContextListener extends ApplicationContextListene
 
 	@Override
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
-		System.out.println("Conext Path " + servletContextEvent.getServletContext().getContextPath());
+		System.out.println("Context Path " + servletContextEvent.getServletContext().getContextPath());
 		super.contextInitialized(servletContextEvent);
 	}
 
