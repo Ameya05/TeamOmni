18cb12cfa01dc51b7590a3e097cbd45064ae9450
Ameya <ameyaadvankar@gmail.com>
2016-11-23 17:32:46 -0500

    #73 Correcting ContextListener file

--- MicroserviceD/src/main/java/org/team/omni/weather/ClusteringAppContextListener.java
+++ MicroserviceD/src/main/java/org/team/omni/weather/ClusteringAppContextListener.java
@@ -3,30 +3,14 @@ package org.team.omni.weather;
 import java.io.File;
 
 import javax.servlet.ServletContextEvent;
-import javax.servlet.annotation.WebListener;
-import javax.servlet.ServletContextListener;
-
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.RetryNTimes;
-
 import org.apache.log4j.Logger;
 /**
  * 
  * @author Ameya Advankar
  *
  */
-@WebListener
-public class ClusteringAppContextListener implements ServletContextListener {
+public class ClusteringAppContextListener extends ApplicationContextListener {
 
-	private CuratorFramework curatorFramework;
-	private ServiceRegistration serviceRegistration;
-	protected String serviceName;
-	protected String servicePath;
-	protected String address;
-	protected int port;
-	protected int maxWorkLoad;
-	
 	static {
 		System.setProperty("my.log", System.getProperty("user.dir") + File.separator + "MicroDlogs.log");
 	}
@@ -43,32 +27,14 @@ public class ClusteringAppContextListener implements ServletContextListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
-		try {
-			serviceRegistration.unregisterService();
-		} catch (ServiceRegistrationException e) {
-			throw new ServiceException(e);
-		}
+		super.contextDestroyed(arg0);
 	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
 		logger.info("Context Path " + servletContextEvent.getServletContext().getContextPath());
-		System.out.println("Context Path " + servletContextEvent.getServletContext().getContextPath());
 		try{
-			//super.contextInitialized(servletContextEvent);
-			String zookeeperAddres = servletContextEvent.getServletContext().getInitParameter("zookeeper");
-			servicePath = servletContextEvent.getServletContext().getContextPath();
-			curatorFramework = CuratorFrameworkFactory.newClient(zookeeperAddres, new RetryNTimes(3, 1000));
-			curatorFramework.start();
-			serviceRegistration = new ServiceRegistration(curatorFramework, address, port, serviceName, servicePath, new InstanceDetails(100));
-			try {
-				serviceRegistration.registerService();
-				//LoadBalancingRequestFilter.setServiceRegistration(serviceRegistration);
-				//LoadBalancingResponseFilter.setServiceRegistration(serviceRegistration);
-			} catch (ServiceRegistrationException e) {
-				e.printStackTrace();
-				throw new ServiceException(e);
-			}
+			super.contextInitialized(servletContextEvent);
 		}
 		catch(Exception e){
 			logger.error("Exception while initializing context",e);
