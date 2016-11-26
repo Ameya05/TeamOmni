612786e0842f939104240cd24b867480ce8d4eb8
Eldho Mathulla <eldhomathulla@gmail.com>
2016-11-25 23:14:36 -0500

    #75 Fixed the setting of illegal load values

--- MicroserviceB/org/team/omni/data/ingestor/DataIngestorService.py
+++ MicroserviceB/org/team/omni/data/ingestor/DataIngestorService.py
@@ -3,6 +3,7 @@ import logging
 import os
 from configparser import ConfigParser
 from datetime import datetime
+import time
 
 from flask import Flask
 from flask import make_response
@@ -12,15 +13,15 @@ from kazoo.client import KazooClient
 from org.team.omni.data.ingestor.NexradHandler import NexradHandler
 from org.team.omni.data.ingestor.ServiceRegistration import ServiceRegistration
 
+logging.basicConfig()
+
 UNIT_WORK_LOAD = 1
 UNIT_WORK_LOAD_DECREASE=UNIT_WORK_LOAD*-1
 
-logging.basicConfig()
-
 LOGGER = logging.getLogger('DataIngestorService')
-service_registration=None
-app = Flask(__name__)
-nexrad_handler = NexradHandler("https://noaa-nexrad-level2.s3.amazonaws.com")
+SERVICE_REGISTRATION=None
+APP = Flask(__name__)
+NEXRAD_HANDLER = NexradHandler("https://noaa-nexrad-level2.s3.amazonaws.com")
 
 
 #Execute flask app taking into account the port
@@ -36,9 +37,9 @@ def execute_flask(flask_app, default_host="0.0.0.0", default_port=65000):
                         default=default_port)
     args = parser.parse_args()
     print(args)
-    global service_registration
-    service_registration=register_service(args.port)
-    service_registration.register_service()
+    global SERVICE_REGISTRATION
+    SERVICE_REGISTRATION=register_service(args.port)
+    SERVICE_REGISTRATION.register_service()
     flask_app.run(host=args.host,port=args.port)
 
 
@@ -63,55 +64,57 @@ def create_response(obj):
     return jsonify({"data": obj})
 
 
-@app.route(
+@APP.route(
     '/nexrad/generate/url/<nexrad_station>/<int:month>/<int:day>/<int:year>/<int:hour>/<int:minute>/<int:seconds>',
     methods=['GET'])
 def genrate_nexrad_file_url(nexrad_station, month, day, year, hour, minute, seconds):
     return create_response(
-        nexrad_handler.fetch_closest_nexrad_file_url(datetime(year, month, day, hour, minute, seconds),
+        NEXRAD_HANDLER.fetch_closest_nexrad_file_url(datetime(year, month, day, hour, minute, seconds),
                                                      nexrad_station).url)
 
 
-@app.route('/nexrad/fetch/years', methods=['GET'])
+@APP.route('/nexrad/fetch/years', methods=['GET'])
 def fetch_years():
-    return create_response(nexrad_handler.fetch_available_years())
+    return create_response(NEXRAD_HANDLER.fetch_available_years())
 
 
-@app.route('/nexrad/fetch/months/<int:year>', methods=['GET'])
+@APP.route('/nexrad/fetch/months/<int:year>', methods=['GET'])
 def fetch_months(year):
-    return create_response(nexrad_handler.fetch_available_months(year))
+    return create_response(NEXRAD_HANDLER.fetch_available_months(year))
 
 
-@app.route('/nexrad/fetch/days/<int:year>/<int:month>', methods=['GET'])
+@APP.route('/nexrad/fetch/days/<int:year>/<int:month>', methods=['GET'])
 def fetch_days(year, month):
-    return create_response(nexrad_handler.fetch_available_days(year, month))
+    return create_response(NEXRAD_HANDLER.fetch_available_days(year, month))
 
 
-@app.route('/nexrad/fetch/stations/<int:year>/<int:month>/<int:day>', methods=['GET'])
+@APP.route('/nexrad/fetch/stations/<int:year>/<int:month>/<int:day>', methods=['GET'])
 def fetch_stations(year, month, day):
-    return create_response(nexrad_handler.fetch_available_stations(year, month, day))
+    return create_response(NEXRAD_HANDLER.fetch_available_stations(year, month, day))
 
 
-@app.errorhandler(500)
+@APP.errorhandler(500)
 def internal_server_error(error):
     LOGGER.exception('Server Error: %s', error)
     resp = make_response("Server Error", 500)
     return resp
 
-@app.before_request
+@APP.before_request
 def before_request():
-    service_registration.update_work_load(UNIT_WORK_LOAD)
+    SERVICE_REGISTRATION.update_work_load(UNIT_WORK_LOAD)
 
-@app.after_request
+@APP.after_request
 def after_request(response):
-    service_registration.update_work_load(UNIT_WORK_LOAD_DECREASE)
+    SERVICE_REGISTRATION.update_work_load(UNIT_WORK_LOAD_DECREASE)
     return response
 
-@app.teardown_request
+@APP.teardown_request
 def tear_down_request(exception):
-    service_registration.update_work_load(UNIT_WORK_LOAD_DECREASE)
+    if exception:
+        SERVICE_REGISTRATION.update_work_load(UNIT_WORK_LOAD_DECREASE)
+
 
 
 
 if __name__ == '__main__':
-    execute_flask(app)
+    execute_flask(APP)
